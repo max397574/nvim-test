6d528f0f1fc02ac3787aa86f7f03d30457b6e653
Lewis Russell <lewis6991@gmail.com>
2022-02-16 14:04:39 +0000

    refactor

--- plugin/nvim_test.lua
+++ plugin/nvim_test.lua
@@ -48,56 +48,63 @@ local function apply_decor(bufnr, lnum, code, stdout)
   })
 end
 
+local function notify_err(msg)
+  vim.notify(msg, vim.log.levels.ERROR)
+end
+
 local function run_nvim_test()
   local name = api.nvim_buf_get_name(0)
   if not name:match('^.*/test/functional/.*$') then
-    print('Buffer is not an nvim functional test file')
+    notify_err('Buffer is not an nvim functional test file')
     return
   end
+
   local lnum = vim.fn.line('.')
   local test, test_lnum = get_test_lnum(lnum)
-  if test then
-    local cbuf = api.nvim_get_current_buf()
-    local stdout = vim.loop.new_pipe(false)
-    local stderr = vim.loop.new_pipe(false)
 
-    local stdout_data = ''
-
-    vim.loop.spawn('make', {
-      args = {
-        'functionaltest',
-        'TEST_FILE='..name,
-        'TEST_FILTER='..test
-      },
-      stdio = { nil, stdout, stderr },
-    },
-      function(code)
-        if stdout then stdout:read_stop() end
-        if stderr then stderr:read_stop() end
-
-        if stdout and not stdout:is_closing() then stdout:close() end
-        if stderr and not stderr:is_closing() then stderr:close() end
-
-        vim.schedule(function()
-          apply_decor(cbuf, test_lnum, code, stdout_data)
-        end)
-      end
-    )
-
-    stdout:read_start(function(_, data)
-      if data then
-        stdout_data = stdout_data..data
-      end
-    end)
-
-    stderr:read_start(function(_, data)
-      if data then
-        stdout_data = stdout_data..data
-      end
-    end)
-  else
-    print('Could not find test')
+  if not test then
+    notify_err('Could not find test')
+    return
   end
+
+  local cbuf = api.nvim_get_current_buf()
+  local stdout = vim.loop.new_pipe(false)
+  local stderr = vim.loop.new_pipe(false)
+
+  local stdout_data = ''
+
+  vim.loop.spawn('make', {
+    args = {
+      'functionaltest',
+      'TEST_FILE='..name,
+      'TEST_FILTER='..test
+    },
+    stdio = { nil, stdout, stderr },
+  },
+    function(code)
+      if stdout then stdout:read_stop() end
+      if stderr then stderr:read_stop() end
+
+      if stdout and not stdout:is_closing() then stdout:close() end
+      if stderr and not stderr:is_closing() then stderr:close() end
+
+      vim.schedule(function()
+        apply_decor(cbuf, test_lnum, code, stdout_data)
+      end)
+    end
+  )
+
+  stdout:read_start(function(_, data)
+    if data then
+      stdout_data = stdout_data..data
+    end
+  end)
+
+  stderr:read_start(function(_, data)
+    if data then
+      stdout_data = stdout_data..data
+    end
+  end)
 end
 
 vim.api.nvim_add_user_command('RunTest', run_nvim_test, {
